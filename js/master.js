c4cfa20fc54112e0201373b49f6a836b2db92925
deflandre <deflandre.vincent@gmail.com>
2019-01-17 22:21:34 +0100

    change home page with info

--- js/master.js
+++ js/master.js
@@ -3,6 +3,8 @@ $(function(){
     if(window.location.hash !== ''){
       $('.panel').removeClass('active');
       $(window.location.hash).addClass('active');
+    }else{
+      $('#home').addClass('active');
     }
 
     $('.panel-button').on('click', function(){
