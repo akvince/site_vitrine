80ed9c94f19ecdcc5c64f773b7a8d932637bc1d1
vdeflandre <deflandre.vincent@gmail.com>
2019-04-04 16:57:51 +0200

    change html presentation to vueJs html

--- js/masterVue.js
+++ js/masterVue.js
@@ -9,3 +9,16 @@ var menu = new Vue({
     ]
   }
 })
+
+var presentation = new Vue({
+  el: '#presentation',
+  data:{
+    infos: [
+      {id: 'Nom & Prénom : ', key: 'Deflandre Vincent'},
+      {id: 'Date de naissance : ', key: '19/07/1990'},
+      {id: 'Profession : ', key: 'Développeur front-end'},
+      {id: 'Affectation : ', key: 'IT-Room'},
+      {id: 'Mission actuelle : ', key: 'La Redoute'},
+    ]
+  }
+})
