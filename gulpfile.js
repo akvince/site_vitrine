ab4972bb5218fec77e7d27af1cb9d21c6d9ed247
vdeflandre <deflandre.vincent@gmail.com>
2019-04-08 15:18:02 +0200

    add concat css

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,15 @@ var gulp = require('gulp');
 gulp.task('default', function () {
     console.log('Hello Gulp!')
 });
+var concatCss = require('gulp-concat-css');
+gulp.task('default', function () {
+  return gulp.src('css/**/*.css')
+    .pipe(concatCss("css/bundle.css"))
+    .pipe(gulp.dest('out/'));
+});
 let cleanCSS = require('gulp-clean-css');
 gulp.task('minify-css', () => {
-  return gulp.src('css/*.css')
+  return gulp.src('css/bundle.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist/'));
 });
