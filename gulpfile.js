a2c5bf06f5135d246b1e2768cd4721435b89db4a
vdeflandre <deflandre.vincent@gmail.com>
2019-04-08 15:07:46 +0200

    change folder css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
-gulp.task('default', function () { 
-    console.log('Hello Gulp!') 
+gulp.task('default', function () {
+    console.log('Hello Gulp!')
 });
 let cleanCSS = require('gulp-clean-css');
 gulp.task('minify-css', () => {
-  return gulp.src('styles/*.css')
+  return gulp.src('css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist/'));
 });
