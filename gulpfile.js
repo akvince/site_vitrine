089ae14fd886decabec6b4ba9437c6f57415b47e
vdeflandre <deflandre.vincent@gmail.com>
2019-04-08 12:47:12 +0000

    add minify css

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,9 @@ var gulp = require('gulp');
 gulp.task('default', function () { 
     console.log('Hello Gulp!') 
 });
+let cleanCSS = require('gulp-clean-css');
+gulp.task('minify-css', () => {
+  return gulp.src('styles/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
+});
