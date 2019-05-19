7693029129b2338eb4f4cc2fcd69ca2fda2aafb7
deflandre <deflandre.vincent@gmail.com>
2019-05-19 17:53:08 +0200

    change for step password

--- js/master.js
+++ js/master.js
@@ -1,5 +1,8 @@
 $(function(){
 
+    var $content = $('.content');
+    var $loadContent = $('.load-content');
+
     if(window.location.hash !== ''){
       $('.panel, .panel-button').removeClass('active');
       $(window.location.hash).addClass('active');
@@ -7,12 +10,40 @@ $(function(){
       $('#home').addClass('active');
     }
 
-    $('.panel-button').on('click', function(){
-      id = $(this).data('id');
-      $('.panel, .panel-button').removeClass('active');
-      $('#' + id).addClass('active');
-      $(this).addClass('active');
+    var showMainContent = function(){
+      $loadContent.hide();
+      $content.show();
+    }
+
+    var hideMainContent = function(){
+      $content.hide();
+      $loadContent.show();
+    }
+
+    var changeHash = function(id){
       window.location.hash = id;
+    }
+
+    var loadPage = function(url){
+      hideMainContent();
+      $.get(url, function(data){
+      	var content = data.documentElement.innerHTML;
+      	$loadContent.html(content);
+      });
+    }
+
+    $('.panel-button').on('click', function(){
+      var id = $(this).data('id');
+      changeHash(id);
+      if(id === "demo"){
+        loadPage('views/plugins/stepPassword.html');
+      }else{
+        showMainContent();
+        $('.panel, .panel-button').removeClass('active');
+        $('#' + id).addClass('active');
+        $(this).addClass('active');
+      }
+
     });
 
 });
