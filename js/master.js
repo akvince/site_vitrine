262012aa21580e847d438ebb086886731f6e5991
akvince <deflandre.vincent@gmail.com>
2019-06-03 17:33:43 +0200

    fixed

--- js/master.js
+++ js/master.js
@@ -1,58 +1,48 @@
 'use strict';
 
-class displayPanel {
+const activeClass = 'active';
+
+class DisplayPanel {
   constructor() {
-    const $content = document.getElementsByClassName('content');
-    const $loadContent = document.getElementsByClassName('load-content');
-    const $panelButton = document.getElementsByClassName('panel-button');
-    const $panel = document.getElementsByClassName('panel');
-    const $home = document.getElementById('home');
+    this.$content = document.getElementsByClassName('content');
+    this.$panelButton = document.getElementsByClassName('panel-button');
+    this.$panel = document.getElementsByClassName('panel');
+    this.$home = document.getElementById('home');
 
-    const removePanelClass = () => {
-      Array.from($panelButton).forEach(function(element) {
-        element.classList.remove('active');
-      });
-      Array.from($panel).forEach(function(element) {
-        element.classList.remove('active');
-      });
-    }
-
-    const showMainContent = () => {
-      $loadContent.hidden = true;
-      $content.hidden = false;
-    }
-
-    const hideMainContent = () => {
-      $content.hidden = true;
-      $loadContent.hidden = false;
-    }
-
-    const changeHash = (id) => {
-      window.location.hash = id;
-    }
-
-    if(window.location.hash !== '' && window.location.hash !== '#demo'){
-      removePanelClass();
-      document.getElementById(window.location.hash.split('#')[1]).classList.add('active');
+    if(!['', '#demo'].includes(window.location.hash)){
+      this.removePanelClass();
+      document.getElementById(window.location.hash.split('#')[1]).classList.add(activeClass);
     }else{
-      $home.classList.add('active');
+      this.$home.classList.add(activeClass);
     }
 
-    Array.from($panelButton).forEach(function(element) {
+    Array.from(this.$panelButton).forEach((element) => {
       element.addEventListener('click', (el) => {
-        var $this = el.currentTarget;
-        var id = $this.getAttribute('data-id');
-        changeHash(id);
+        const $this = el.currentTarget;
+        const id = $this.getAttribute('data-id');
+        this.changeHash(id);
         if(id === "demo"){
           window.location.href = 'views/demo.html';
         }else{
-          showMainContent();
-          removePanelClass();
-          document.getElementById(id).classList.add('active');
-          $this.classList.add('active');
+          this.removePanelClass();
+          document.getElementById(id).classList.add(activeClass);
+          $this.classList.add(activeClass);
         }
       });
     });
+  }
+
+  removePanelClass(){
+    Array.from(this.$panelButton).forEach((element) =>{
+      element.classList.remove(activeClass);
+    });
+    Array.from(this.$panel).forEach((element) =>{
+      element.classList.remove(activeClass);
+    });
+  }
+
+  changeHash(id){
+    window.location.hash = id;
   }
 
 };
--- js/plugins/passwordStep.js
+++ js/plugins/passwordStep.js
@@ -1,107 +1,92 @@
 'use strict';
 
-class stepPassword {
+class StepPassword {
   constructor() {
-    console.log('init password validation step');
-
-    var $passwordInput = document.getElementById('passwordInput');
-    var $passwordInputVal = $passwordInput.value;
-    var successTick = 'fa-check';
-    var unsuccessTick = 'fa-times';
-    var passwordMinLength = 8;
-
-    /*construct dom to display step password*/
-    var constructDom = function(){
-        $passwordInput.insertAdjacentHTML('afterend', '<div id="containerStepValidation" class="row"></div>');
-        var $containerStepValidation = document.getElementById('containerStepValidation');
-        $containerStepValidation.insertAdjacentHTML('beforeend', '<div class="col-xs-12">Etape de validation de mot de passe</div>');
-        $containerStepValidation.insertAdjacentHTML('beforeend', '<div id="passwordStepLength" class="col-xs-12 col-sm-6 step-label"><span class="password-step-tick fas fa-times"></span>' + 'minimum 8 caratères</div>');
-        $containerStepValidation.insertAdjacentHTML('beforeend', '<div id="passwordStepNumber" class="col-xs-12 col-sm-6 step-label"><span class="password-step-tick fas fa-times"></span>' + 'contient un chiffre</div>');
-        $containerStepValidation.insertAdjacentHTML('beforeend', '<div id="passwordStepLetter" class="col-xs-12 col-sm-6 step-label"><span class="password-step-tick fas fa-times"></span>' + 'contient une lettre</div>');
-        $containerStepValidation.insertAdjacentHTML('beforeend', '<div id="passwordStepCapital" class="col-xs-12 col-sm-6 step-label"><span class="password-step-tick fas fa-times"></span>' + 'contient une majuscule</div>');
-    }
-
-    /*change tick el in green if step is valide*/
-    var checkedStep = function($tickContainer){
-        const $tick = $tickContainer.getElementsByClassName('password-step-tick')
-        $tick[0].classList.remove(unsuccessTick)
-        $tick[0].classList.add(successTick);
-    }
-
-    /*change tick el in black if step is not valide*/
-    var uncheckedStep = function($tickContainer){
-        const $tick = $tickContainer.getElementsByClassName('password-step-tick')
-        $tick[0].classList.remove(successTick)
-        $tick[0].classList.add(unsuccessTick);
-    }
-
-    /*
-     * method check if condition is
-     * exemple => $selector = "#selector" and regexp = /[A-Z]/
-    */
-    var check = function(selector, regexp){
-        const $iconStep = document.getElementById(selector);
-        if(regexp.test($passwordInput.value)){
-            checkedStep($iconStep);
-        }else{
-            uncheckedStep($iconStep);
-        }
-    }
-
-    /*check if password contains one letter*/
-    var checkLetter = function(){
-        check('passwordStepLetter', /[a-z]/);
-    }
-
-    /*check if password contains one number*/
-    var checkNumber = function(){
-        check('passwordStepNumber', /\d+/);
-    }
-
-    /*check if password contains one capital letter*/
-    var checkCapital = function(){
-        check('passwordStepCapital', /[A-Z]/);
-    }
-
-    /*check if password contains 8 or more charaters*/
-    var checkLength = function(){
-        const $iconStepLength = document.getElementById('passwordStepLength');
-        if($passwordInput.value.length >= passwordMinLength){
-            checkedStep($iconStepLength);
-        }else{
-            uncheckedStep($iconStepLength);
-        }
-    }
-
-    /*verify all status step*/
-    const checkAllStep = function(){
-        checkLetter();
-        checkCapital();
-        checkNumber();
-        checkLength();
-    }
-
-        // Public methods
-
+    this.$passwordInput = document.getElementById('passwordInput');
+    this.successTick = 'fa-check';
+    this.unsuccessTick = 'fa-times';
+    this.passwordMinLength = 8;
+    this.constructDom();
     // Events
-    $passwordInput.addEventListener('keyup', function(){
-        checkAllStep();
+    this.$passwordInput.addEventListener('keyup', () => {
+        this.checkAllStep();
     });
-
-    // Init
-    constructDom();
-
     /*style*/
     const $stepLabel = document.getElementsByClassName('step-label');
     const $passwordStepTick = document.getElementsByClassName('password-step-tick');
-    Array.from($stepLabel).forEach(function(element) {
-        var $this = element;
-        $this.style.fontSize = '14px';
-    });
-    Array.from($passwordStepTick).forEach(function(element) {
-        var $this = element;
-        $this.style.verticalAlign = 'middle';
-    });
+    Array.from($stepLabel).forEach(element => element.style.fontSize = '14px');
+    Array.from($passwordStepTick).forEach(element => element.style.verticalAlign = 'middle');
+  }
+
+  /*construct dom to display step password*/
+  constructDom(){
+      this.$passwordInput.insertAdjacentHTML('afterend', '<div id="containerStepValidation" class="row"></div>');
+      const $containerStepValidation = document.getElementById('containerStepValidation');
+      $containerStepValidation.insertAdjacentHTML('beforeend', '<div class="col-xs-12">Etape de validation de mot de passe</div>');
+      $containerStepValidation.insertAdjacentHTML('beforeend', '<div id="passwordStepLength" class="col-xs-12 col-sm-6 step-label"><span class="password-step-tick fas fa-times"></span>' + 'minimum 8 caratères</div>');
+      $containerStepValidation.insertAdjacentHTML('beforeend', '<div id="passwordStepNumber" class="col-xs-12 col-sm-6 step-label"><span class="password-step-tick fas fa-times"></span>' + 'contient un chiffre</div>');
+      $containerStepValidation.insertAdjacentHTML('beforeend', '<div id="passwordStepLetter" class="col-xs-12 col-sm-6 step-label"><span class="password-step-tick fas fa-times"></span>' + 'contient une lettre</div>');
+      $containerStepValidation.insertAdjacentHTML('beforeend', '<div id="passwordStepCapital" class="col-xs-12 col-sm-6 step-label"><span class="password-step-tick fas fa-times"></span>' + 'contient une majuscule</div>');
+  }
+
+  /*change tick el in green if step is valide*/
+  checkedStep($tickContainer){
+      const $tick = $tickContainer.getElementsByClassName('password-step-tick')[0];
+      $tick.classList.remove(this.unsuccessTick);
+      $tick.classList.add(this.successTick);
+  }
+
+  /*change tick el in black if step is not valide*/
+   uncheckedStep($tickContainer){
+      const $tick = $tickContainer.getElementsByClassName('password-step-tick')[0];
+      $tick.classList.remove(this.successTick);
+      $tick.classList.add(this.unsuccessTick);
+  }
+
+  /*
+   * method check if condition is
+   * exemple => $selector = "#selector" and regexp = /[A-Z]/
+  */
+   check(selector, regexp){
+      const $iconStep = document.getElementById(selector);
+      if(regexp.test(this.$passwordInput.value)){
+          this.checkedStep($iconStep);
+      }else{
+          this.uncheckedStep($iconStep);
+      }
+  }
+
+  /*check if password contains one letter*/
+  checkLetter(){
+      this.check('passwordStepLetter', /[a-z]/);
+  }
+
+  /*check if password contains one number*/
+  checkNumber(){
+      this.check('passwordStepNumber', /\d+/);
+  }
+
+  /*check if password contains one capital letter*/
+  checkCapital(){
+      this.check('passwordStepCapital', /[A-Z]/);
+  }
+
+  /*check if password contains 8 or more charaters*/
+  checkLength(){
+      const $iconStepLength = document.getElementById('passwordStepLength');
+      if(this.$passwordInput.value.length >= this.passwordMinLength){
+          this.checkedStep($iconStepLength);
+      }else{
+          this.uncheckedStep($iconStepLength);
+      }
+  }
+
+  /*verify all status step*/
+  checkAllStep(){
+      this.checkLetter();
+      this.checkCapital();
+      this.checkNumber();
+      this.checkLength();
   }
 
 }
