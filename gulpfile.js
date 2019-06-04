5cb8f8ee9182c527d2574e7c04d257f7976c0cd6
akvince <deflandre.vincent@gmail.com>
2019-06-04 16:26:10 +0200

    add concat and minify css

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,19 @@ let gulp = require('gulp');
 gulp.task('default', function () {
     console.log('Hello Gulp!')
 });
+
 let concatCss = require('gulp-concat-css');
 gulp.task('concat-css', function () {
   return gulp.src('./css/*.css')
     .pipe(concatCss("./css/bundle.css"))
     .pipe(gulp.dest('./'));
 });
+
 let cleanCSS = require('gulp-clean-css');
 gulp.task('minify-css', () => {
   return gulp.src('./css/bundle.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('buildCss', gulp.series('concat-css', 'minify-css'));
